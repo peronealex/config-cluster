7a1a9ec3e4b91bbb7220781a4dec8aab8c1e5cce
Alessio Perone <alessio.perone@lexisnexisrisk.com>
2021-01-08 12:37:56 +0000

    adding variables

--- variable.tf
+++ variable.tf
@@ -2,3 +2,13 @@ variable "cluster_name" {
     default = "Shared Autoscaling"
     description = "name of cluster"
 }
+
+variable "spark_version" {
+    default = "6.6.x-scala2.11"
+    description = "version in spark"
+}
+
+variable "autotermination_minutes" {
+    default = "20"
+    description = "termination in minutes"
+}
